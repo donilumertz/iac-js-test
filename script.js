f8962e64d14b8e9f85e9326936a1e72bff6571ef
Cassio Costa <cassio@bitfacil.com>
2020-06-16 21:35:28 -0300

    ifs e onclick

--- script.js
+++ script.js
@@ -7,6 +7,12 @@ console.log("negando a " + s);
 var s = a && b;
 console.log(" a AND b " + s);
 
+if(a && b){
+    console.log("passou no este do end");
+}else{
+    console.log("errou");
+}
+
 var s = a || b;
 console.log(" a OR b " + s);
 
@@ -15,6 +21,41 @@ console.log("a xor b (2) " + s);
 
 var s = (a || b) && !(a && b);
 console.log("a xor b (3) " + s);
+
+
+var temperatura = 20;
+
+if(temperatura>=10 || temperatura <=15){
+    console.log("temperatura de boas pro gaudério");
+}else if(temperatura>=16 && temperatura<=30){
+    console.log("ta quentinho");
+}
+else{
+    console.log("da pra tomar banho na rua ainda sendo gaudério");
+}
+
+function pegarVariavel(){
+    var variavel = document.getElementById('variavel').value;
+    console.log(variavel);
+    document.getElementById('result').innerHTML = "o conteudo é " + variavel;
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 /**var nome = "João da Silva";
 
